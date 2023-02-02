def1eb25c5b9f4dcff69a8fe0bd4eb9e0b2b89df
theUmeshC <umesh.c@celestialsys.com>
2023-02-02 15:45:01 +0530

    association of tables

--- Backend/Modules/Connections.js
+++ Backend/Modules/Connections.js
@@ -2,47 +2,74 @@ import { Sequelize } from "sequelize";
 
 import sequelize from "../utils/database.js";
 
-export const private_connection = sequelize.define("private_connects",{
+export const private_connection = sequelize.define(
+  "individual_connect",
+  {
     sender_id: {
-        type : Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
     receiver_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
-    chat_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-    }
-});
+  },
+  {
+    timestamp: false,
+  }
+);
 
-export const group_connection = sequelize.define("group_connects",{
+export const group_connection = sequelize.define(
+  "group_connect",
+  {
     user_id: {
-        type : Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
     group_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
-    chat_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-    }
-});
+  },
+  {
+    timestamp: false,
+  }
+);
 
-export const chats = sequelize.define('chats', {
+export const chat = sequelize.define(
+  "chat",
+  {
     chat_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
     message: {
-        type: Sequelize.TEXT,
-        allowNull: false,
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
+  },
+);
+
+chat.hasMany(private_connection, {
+  foreignKey: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
 });
+
+private_connection.belongsTo(chat);
+
+
+chat.hasMany(group_connection, {
+    foreignKey: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+  });
+  
+  group_connection.belongsTo(chat);
